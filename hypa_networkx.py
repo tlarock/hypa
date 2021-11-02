aedc3de319fe2802441248cfedd1908f0f6d125f
Tim LaRock <timothylarock@gmail.com>
2021-11-02 19:19:55 -0400

    Added separate classes that inherit from a simple baseclass.

---
+++ hypa_networkx.py
@@ -0,0 +1,170 @@
+import networkx as nx
+import numpy as np
+from scipy.sparse import dok_matrix
+from computexi import fitXi
+from scipy.stats import hypergeom
+from hypa import Hypa
+
+class HypaNX(Hypa):
+
+    def __init__(self, input_file, k, xitol=1e-2, observed_only=True,frequency=False, verbose=True):
+        '''
+        Accepts an ngram filename and integer k and computes a kth-order
+        HON from ngram, including unobserved but possible edges. This
+        is necessary for correctly computing the xi matrix in the
+        next step.
+
+        Parameters
+        ---------
+        input_file (str): path to the ngram file
+        k (int): order of the HON
+        frequency (bool): if True, ngram file rows
+                        end with a value to be interpreted
+                        as the frequency of the path
+        verbose (bool): if True, print more stuff
+        '''
+        super().__init__(implementation='scipy')
+        self.input_file = input_file
+        self.k = k
+        self.frequency = frequency
+        self.verbose = verbose
+        self.xitol = xitol
+
+        if verbose:
+            print("Constructing HON.")
+        self.hypa_from_ngram()
+        if verbose:
+            print("Constructing xi.")
+        self.construct_xi()
+        if verbose:
+            print("Computing pvals.")
+        self.compute_pvals()
+
+    def hypa_from_ngram(self):
+        # Read ngram file
+        hypa_net = nx.DiGraph()
+        first_order = nx.DiGraph()
+
+        lines_read = 0
+        interval_lines = 0
+        interval = 100_000
+        # Read the input
+        with open(self.input_file) as fin:
+            for line in fin:
+                lines_read += 1
+                interval_lines += 1
+                if interval_lines == interval and self.verbose:
+                    print(f'{lines_read} lines read.', flush=True)
+                    interval_lines = 0
+                # Strip trailing whitespace
+                line = line.strip().split(',')
+
+                # parse path
+                if self.frequency:
+                    path = line[0:-1]
+                    freq = float(line[-1])
+                else:
+                    path = line
+                    freq = 1
+
+                # Add first order edges (always)
+                for i in range(1, len(path)):
+                    edge = (path[i-1], path[i])
+                    if edge not in first_order.edges:
+                        first_order.add_edge(path[i-1], path[i])
+
+                # Skip a path if its length is less than k
+                k = self.k
+                if len(path)-1 < k:
+                    continue
+
+                # Add all nodes and edges
+                for i in range(0, len(path)-k):
+                    u, v = ','.join(path[i:i+k]), ','.join(path[i+1:i+k+1])
+                    if (u, v) in hypa_net.edges:
+                        hypa_net.edges[(u, v)]['weight'] += freq
+                    else:
+                        hypa_net.add_edge(u, v, weight=freq)
+
+        # To compute xi correctly, I need to include
+        # all of the possible edges that had 0 frequency.
+        edges_to_add = []
+        for node in hypa_net.nodes():
+            splitnode = node.split(',')
+            prefix = splitnode[1:]
+            target = splitnode[-1]
+            for successor in first_order.successors(target):
+                new_node = prefix + [successor]
+                new_node_str = ','.join(new_node)
+                if (node, new_node_str) not in hypa_net.edges():
+                    edges_to_add.append((node, new_node_str, {'weight': 0}))
+        hypa_net.add_edges_from(edges_to_add)
+
+        self.hypa_net = hypa_net
+
+    def construct_xi(self):
+        '''
+        Construct the xi matrix from a hypa_net.
+
+        Note: Relies on fitXi in computexi.py.
+        '''
+        N = len(self.hypa_net.nodes())
+        adj = dok_matrix((N, N))
+        xi = dok_matrix((N, N))
+        # compute Xi for every edge in hypa_net
+        node_to_idx = dict()
+        idx = 0
+        for u, v, edat in self.hypa_net.edges(data=True):
+            xival = self.hypa_net.out_degree(u, weight='weight') *\
+                    self.hypa_net.in_degree(v, weight='weight')
+            if u not in node_to_idx:
+                node_to_idx[u] = idx
+                idx += 1
+            if v not in node_to_idx:
+                node_to_idx[v] = idx
+                idx += 1
+
+            xi[node_to_idx[u], node_to_idx[v]] = xival
+            edat['xival'] = xival
+            adj[node_to_idx[u], node_to_idx[v]] = edat['weight']
+
+        xi = fitXi(adj, xi, tol=self.xitol,
+                   sparsexi=True, verbose=self.verbose)
+        xi = xi.tocsr()
+        for u, v, edat in self.hypa_net.edges(data=True):
+            edat['xival_orig'] = float(edat['xival'])
+            edat['xival'] = xi[node_to_idx[u], node_to_idx[v]]
+
+        self.adj = adj
+        self.xi = xi
+        self.node_to_idx = node_to_idx
+
+    def compute_pvals(self):
+        '''
+        Compute pvalues for the edges in hypa_net. If observed_only is True,
+        only compute pvalues for edges with weight > 0.
+        '''
+        # loop over all edges in hypa_net
+        xisum = self.xi.sum()
+        adjsum = self.adj.sum()
+        edges_to_remove = []
+        for u, v, edat in self.hypa_net.edges(data=True):
+            if self.observed_only and edat['weight'] == 0:
+                edges_to_remove.append((u, v))
+                continue
+            pval = self.compute_hypa(edat['weight'], edat['xival'], xisum, adjsum)
+            edat['log-pval'] = pval
+            edat['pval'] = np.exp(pval)
+
+        if len(edges_to_remove) > 0:
+            self.hypa_net.remove_edges_from(edges_to_remove)
+
+
+    def compute_hypa(obs_freq, xi, total_xi, total_observations, log_p=True):
+        """
+        Compute hypa score. Only using the scipy implementation here.
+        """
+        if log_p:
+            return hypergeom.logcdf(obs_freq, total_xi, xi, total_observations)
+        else:
+            return hypergeom.cdf(obs_freq, total_xi, xi, total_observations)
---
+++ hypa_pathpy.py
@@ -0,0 +1,453 @@
+import numpy as np
+import scipy.sparse as sp
+import pathpy as pp
+from random import shuffle
+from .computexi import computeXiHigherOrder, fitXi, xi_matrix
+from .hypa import Hypa
+
+
+class HypaPP(Hypa):
+    '''
+    Class for computing hypa scores on a DeBruijn graph given pathway data.
+    '''
+    def __init__(self, implementation):
+        """
+        Initialize Hypa object.
+        """
+        super(implementation)
+
+    @classmethod
+    def from_paths(cls, paths, k, implementation='julia', **kwargs):
+        ''' Accept a pathpy.Paths object
+
+        parameters
+        -----------
+        paths: Paths
+            Paths object containing the pathway data.
+        k: int
+            Path length of interest (order). Measured in
+                    terms of number of edges.
+        implementation: str
+            Which implementation to use to compute the
+                hypergeometric distribution. Options are
+                julia, rpy2, scipy.
+        **kwargs: Keyword arguments to construct_hypa_network
+
+        returns
+        -------
+        self: Hypa
+            The instantiated Hypa object constructed from paths
+
+        '''
+        self = cls(implementation=implementation)
+        self.paths = paths
+        self.construct_hypa_network(k=k, **kwargs)
+
+        return self
+
+    @classmethod
+    def from_ngram(cls, input_file, k, frequency=False, implementation='julia', xitol=1e-2, sparsexi=True, verbose=True):
+        '''
+        Accepts a filename to an ngram file where each row is a path. If frequency is True,
+        an integer value at the end of each row counts the total number of times the path
+        occurs in the dataset. Otherwise paths may occur multiple times.
+
+        Useful for cases where ngram file is too large to conveniently read into a paths object.
+        '''
+
+        self = cls(implementation=implementation)
+        self.paths = None
+        self.hypa_net = pp.Network(directed=True)
+        first_order = pp.Network(directed=True)
+
+        lines_read = 0
+        interval_lines = 0
+        interval = 100_000
+        # Read the input
+        with open(input_file) as fin:
+            for line in fin:
+                lines_read += 1
+                interval_lines += 1
+                if interval_lines == interval and verbose:
+                    print(f'{lines_read} lines read.', flush=True)
+                    interval_lines = 0
+                # Strip trailing whitespace
+                line = line.strip().split(',')
+
+                # parse path
+                if frequency:
+                    path = line[0:-1]
+                    freq = int(line[-1])
+                else:
+                    path = line
+                    freq = 1
+
+                # Add first order edges (always)
+                for i in range(1, len(path)):
+                    edge = (path[i-1], path[i])
+                    if edge not in first_order.edges:
+                        first_order.add_edge(path[i-1], path[i])
+
+                # Skip a path if its length is less than k
+                if len(path)-1 < k:
+                    continue
+
+                # Add all edges
+                for i in range(0, len(path)-k):
+                    u, v = ','.join(path[i:i+k]), ','.join(path[i+k:])
+                    if (u,v) in self.hypa_net.edges:
+                        self.hypa_net.edges[(u,v)]['weight'] += freq
+                    else:
+                        self.hypa_net.add_edge(u, v, weight=freq)
+
+
+        if verbose:
+            print(f"Computing the k={k} order Xi")
+
+        self.adjacency = self.hypa_net.adjacency_matrix()
+        for node in self.hypa_net.nodes:
+            source = node
+            # If this node has 0 outweight, it will have all 0 xi so can be ignored
+            if self.hypa_net.nodes[source]['outweight'] == 0:
+                continue
+
+            node_as_path = node.split(',')
+            fo_neighbors = first_order.successors[node_as_path[-1]]
+            for neighbor in fo_neighbors:
+                # ToDo separator in split
+                target = ','.join(node_as_path[1:]) + f',{neighbor}'
+                # If target is not a node or has 0 inweight, it will have 0 xi so can be ignored
+                if target in self.hypa_net.nodes:
+                    # Splitting in to 2 lines to avoid defaultdict issue
+                    if self.hypa_net.nodes[target]['inweight'] > 0:
+                        xi_val = self.hypa_net.nodes[source]['outweight'] * self.hypa_net.nodes[target]['inweight']
+                        self.hypa_net.edges[(source, target)]['xival'] = xi_val
+                        if 'weight' not in self.hypa_net.edges[(source, target)]:
+                            self.hypa_net.edges[(source, target)]['weight'] = 0.0
+
+        self.Xi = xi_matrix(self.hypa_net)
+        self.Xi = fitXi(self.adjacency, self.Xi, sparsexi=sparsexi, tol=xitol, verbose=verbose)
+
+        if verbose:
+            print("Computing HYPA scores")
+        reverse_name_dict = {val:key for key,val in self.hypa_net.node_to_name_map().items()}
+        adjsum = self.adjacency.sum()
+        xisum = self.Xi.sum()
+        for u,v,xival in zip(self.Xi.row, self.Xi.col, self.Xi.data):
+            source, target = reverse_name_dict[u], reverse_name_dict[v]
+            pval = self.compute_hypa(self.adjacency[u,v], xival, xisum, adjsum, log_p=True)
+            if xival > 0:
+                try:
+                    self.hypa_net.edges[(source, target)]['pval'] = pval
+                    self.hypa_net.edges[(source, target)]['xival'] = xival
+                except Exception as e:
+                    attr = {'weight': 0.0, 'pval':pval, 'xi':xival}
+                    self.hypa_net.add_edge(source, target, **attr)
+
+        # TODO: Saving first order in this case; should make a choice generally
+        self.first_order = first_order
+        return self
+
+
+
+    @classmethod
+    def from_graph_file(cls, input_file, implementation='julia', xitol=1e-2, sparsexi=True, verbose=True):
+        ''' Read a file to initialize the hypa object. We assume
+            the file represents a kth order graph and each line
+            is of the form
+                        u_1,u_2,...,u_{k+1},freq
+            Note that the nodes and edges must be parsed from
+            this representation.
+
+            parameters
+            ----------
+            input_file: str
+                path to file storing the higher-order graph
+            implementation: str
+                Which implementation to use to compute the
+                    hypergeometric distribution. Options are
+                    julia, rpy2, scipy.
+            xitol: float
+                Error tolerance for redistirbution of Xi values
+            sparsexi: boolean
+                if True, use a scipy.sparse matrix
+            verbose: boolean
+                if True, print more messages
+            returns
+            -------
+            self: Hypa
+                The instantiated Hypa object constructed from paths
+        '''
+        self = cls(implementation=implementation)
+        self.paths = None
+        self.hypa_net = pp.Network(directed=True)
+        first_order = pp.Network(directed=True)
+
+        if verbose:
+            print("Reading file.")
+        with open(input_file, 'r') as fin:
+            # Can I sort the input so that I can always compute Xi? Could put M at the top of a file
+            for line in fin:
+                line = line.strip()
+                line_list = line.split(',')
+                path = line_list[0:-1]
+                # Inferring k
+                k = len(path)-1
+                freq = int(line_list[-1])
+                u, v = ','.join(path[0:k]), ','.join(path[1:])
+                self.hypa_net.add_edge(u, v, weight=freq)
+                for i in range(1, len(path)):
+                    edge = (path[i-1], path[i])
+                    if edge not in first_order.edges:
+                        first_order.add_edge(path[i-1], path[i])
+
+        if verbose:
+            print(f"Computing the k={k} order Xi")
+        self.adjacency = self.hypa_net.adjacency_matrix()
+        for node in self.hypa_net.nodes:
+            source = node
+            # If this node has 0 outweight, it will have all 0 xi so can be ignored
+            if self.hypa_net.nodes[source]['outweight'] == 0:
+                continue
+
+            node_as_path = node.split(',')
+            fo_neighbors = first_order.successors[node_as_path[-1]]
+            for neighbor in fo_neighbors:
+                # ToDo separator in split
+                target = ','.join(node_as_path[1:]) + f',{neighbor}'
+                # If target is not a node or has 0 inweight, it will have 0 xi so can be ignored
+                if target in self.hypa_net.nodes:
+                    # Splitting in to 2 lines to avoid defaultdict issue
+                    if self.hypa_net.nodes[target]['inweight'] > 0:
+                        xi_val = self.hypa_net.nodes[source]['outweight'] * self.hypa_net.nodes[target]['inweight']
+                        self.hypa_net.edges[(source, target)]['xival'] = xi_val
+                        if 'weight' not in self.hypa_net.edges[(source, target)]:
+                            self.hypa_net.edges[(source, target)]['weight'] = 0.0
+
+        self.Xi = xi_matrix(self.hypa_net)
+        self.Xi = fitXi(self.adjacency, self.Xi, sparsexi=sparsexi, tol=xitol, verbose=verbose)
+
+        if verbose:
+            print("Computing HYPA scores")
+        reverse_name_dict = {val:key for key,val in self.hypa_net.node_to_name_map().items()}
+        adjsum = self.adjacency.sum()
+        xisum = self.Xi.sum()
+        for u,v,xival in zip(self.Xi.row, self.Xi.col, self.Xi.data):
+            source, target = reverse_name_dict[u], reverse_name_dict[v]
+            pval = self.compute_hypa(self.adjacency[u,v], xival, xisum, adjsum, log_p=True)
+            if xival > 0:
+                try:
+                    self.hypa_net.edges[(source, target)]['pval'] = pval
+                    self.hypa_net.edges[(source, target)]['xival'] = xival
+                except Exception as e:
+                    attr = {'weight': 0.0, 'pval':pval, 'xi':xival}
+                    self.hypa_net.add_edge(source, target, **attr)
+
+        return self
+
+    def initialize_xi(self, k=2, sparsexi=True, redistribute=True, xifittol=1e-2, verbose=True):
+        r"""
+        Initialize the xi matrix for the paths object.
+
+        Parameters
+        ----------
+        k: int
+            Order to compute xi at
+        sparsexi: logical
+            If True, use scipy sparse matrices for computations. Default True.
+        redistribute: logical
+            If True, call fitXi on the matrix to redistribute excess weights. Default True.
+        xifittol: float
+            Error tolerance in expected weight for fitXi call. Ignored if redistribute is False.
+        verbose: logical
+            If True, print out details of what is happening.
+
+
+        """
+        if verbose:
+            print('Computing the k={} order Xi...'.format(k))
+
+        # Assign k
+        self.k = k
+
+        # Compute Xi. Also returns a network object.
+        self.Xi, self.hypa_net = computeXiHigherOrder(self.paths, k=self.k, sparsexi=sparsexi)
+        self.adjacency = self.hypa_net.adjacency_matrix()
+
+        if redistribute:
+            if verbose:
+                print('Fitting Xi...')
+
+            # Fit the Xi matrix to preserve expected in/out weights
+            self.Xi = fitXi(self.adjacency, self.Xi, sparsexi=sparsexi, tol=xifittol, verbose=verbose)
+        self.adjacency = self.hypa_net.adjacency_matrix()
+
+
+    def construct_hypa_network(self, k=2, log=True, sparsexi=True, redistribute=True, xifittol=1e-2, verbose=True):
+        """
+        Function to compute the significant pathways from a Paths object.
+
+        parameters
+        -----------
+
+        paths: Paths
+            Paths object containing the pathway data.
+        order: int
+            The order at which the significance should be computed.
+        log: logical
+            If True, compute and return pvals as log(p)
+
+
+        Return
+        -----------
+
+        hypa_net: pathpy.Network
+            pathpy network representing transitions at order _k_. Edge data
+                includes the observed frequency, hypa score, and xi value.
+        network: pathpy.Network
+            pathpy network corresponding to the higher order Xi matrix
+        Xi: numpy.ndarray
+            The Xi matrix computed based on the path data
+
+
+        """
+        def add_edge(u, v, xival, xisum, adjsum, reverse_name_dict):
+            source, target = reverse_name_dict[u],reverse_name_dict[v]
+            pval = self.compute_hypa(self.adjacency[u,v], xival, xisum, adjsum, log_p=True)
+            if xival > 0:
+                try:
+                    self.hypa_net.edges[(source, target)]['pval'] = pval
+                    # xival may have been updated by fitXi, make sure it is set correctly here
+                    self.hypa_net.edges[(source, target)]['xival'] = xival
+                except Exception as e:
+                    # add edge myself
+                    attr = {'weight': 0.0, 'pval':pval, 'xi':xival}
+                    #self.hypa_net.add_edge(source, target, **attr)
+                    self.hypa_net.edges[(source,target)] = attr
+            return 1
+
+
+        # assign k
+        self.k = k
+
+        # create network and Xi matrix
+        self.initialize_xi(k=self.k, sparsexi=sparsexi, redistribute=redistribute, xifittol=xifittol, verbose=verbose)
+
+        xisum = self.Xi.sum()
+        xicoo = sp.coo_matrix(self.Xi)
+
+        node_name_map = self.hypa_net.node_to_name_map()
+        reverse_name_dict = {val:key for key,val in node_name_map.items()}
+        adjsum = self.adjacency.sum()
+        for u,v,xival in zip(xicoo.row, xicoo.col, xicoo.data):
+            if xival > 0:
+                add_edge(u, v, xival, xisum, adjsum, reverse_name_dict)
+            elif self.adjacency[u,v] == 0:
+                del self.hypa_net.edges[(reverse_name_dict[u],reverse_name_dict[v])]
+
+    def compute_hypa(self, obs_freq, xi, total_xi, total_observations, log_p=True):
+        """
+        Compute hypa score using appropriate implementation.
+        """
+        if self.implementation == 'julia':
+            hy = Hypergeometric(total_observations, total_xi - total_observations, xi)
+            if log_p:
+                return logcdf(hy, obs_freq)
+            else:
+                return cdf(hy, obs_freq)
+        elif self.implementation == 'rpy2':
+            return self.rphyper(obs_freq, xi, total_xi-xi, total_observations, log_p=log_p)[0]
+        elif self.implementation == 'scipy':
+            if log_p:
+                return hypergeom.logcdf(obs_freq, total_xi, xi, total_observations)
+            else:
+                return hypergeom.cdf(obs_freq, total_xi, xi, total_observations)
+
+    def draw_sample(self, implementation=None, seed=None):
+        r"""
+        Draw a sample from the hypergeometric ensemble.
+
+        Drawing a sample is implemented as sampling a weight
+            for each edge. Sampled weights are stored in the
+            self.hypa_net.edges dictionary with the key
+            'sampled_weight'.
+
+        NOTE: Drawing new sample overwrites old values of 'sampled_weight'
+
+        Currently implemented for Juilia and rpy2.
+
+        Implementations inspired by numpy.random.Generator.multivariate_hypergeometric ("marginals" option):
+            https://numpy.org/devdocs/reference/random/generated/numpy.random.Generator.multivariate_hypergeometric.html
+
+        Parameters
+        --------
+
+        Returns
+        --------
+
+        """
+        if implementation is None:
+            implementation = self.implementation
+
+        if implementation == 'julia':
+            self.draw_sample_julia()
+        elif implementation ==  'rpy2':
+            self.draw_sample_rpy2()
+        elif implementation == 'scipy':
+            self.draw_sample_numpy(seed)
+
+    def draw_sample_julia(self):
+        total_xi = self.Xi.sum()
+        # Sample once per existing edge
+        num_samples = self.adjacency.sum()
+        xi_accum = 0
+        # Loop over the edges
+        for i, edge in enumerate(self.hypa_net.edges):
+            if num_samples < 1:
+                break
+            xi = self.hypa_net.edges[edge]['xival']
+            xi_accum += xi
+            #if i < len(edges_list) - 1:
+            if i < len(self.hypa_net.edges) - 1:
+
+                # Hypergeometric distribution for a population with s successes and f failures, and a sequence of n trials.
+                #Hypergeometric(s, f, n)
+                hy = Hypergeometric(xi, total_xi - xi_accum, num_samples)
+                sample = rand(hy)
+                self.hypa_net.edges[edge]['sampled_weight'] = sample
+            else:
+                sample = int(num_samples)
+                self.hypa_net.edges[edge]['sampled_weight'] = sample
+
+            num_samples -= sample
+
+    def draw_sample_rpy2(self):
+        total_xi = self.Xi.sum()
+        num_samples = self.adjacency.sum()
+        xi_accum = 0
+
+        edges_list = list(self.hypa_net.edges)
+        shuffle(edges_list)
+        for i, edge in enumerate(edges_list):
+            if num_samples < 1:
+                break
+            xi = self.hypa_net.edges[edge]['xival']
+            xi_accum += xi
+            if i < len(edges_list) - 1:
+                sample = self.rrhyper(1, xi, total_xi-xi_accum, num_samples)[0]
+                self.hypa_net.edges[edge]['sampled_weight'] = sample
+            else:
+                sample = int(num_samples)
+                self.hypa_net.edges[edge]['sampled_weight'] = sample
+
+            num_samples -= sample
+
+    def draw_sample_numpy(self, seed=None):
+        # Read xi values in order of hypa_net.edges
+        colors = [int(data['xival']) for _, data in self.hypa_net.edges.items() if data['xival'] > 0]
+        nsamples = int(self.adjacency.sum())
+        gen = np.random.Generator(np.random.PCG64(seed))
+        variates = gen.multivariate_hypergeometric(colors, nsamples, method='count')
+        # Write sampled_weights in order of hypa_net.edges
+        for i, edge in enumerate(self.hypa_net.edges):
+            self.hypa_net.edges[edge]['sampled_weight'] = variates[i]
