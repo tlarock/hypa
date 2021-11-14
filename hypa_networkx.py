0412d90e4e05dcbb43386b9dd2b91e542b71d807
Tim LaRock <timothylarock@gmail.com>
2021-11-14 11:27:37 -0500

    Fixed weights.

--- hypa_networkx.py
+++ hypa_networkx.py
@@ -164,9 +164,9 @@ class HypaNX():
                 for i in range(1, len(path)):
                     edge = (path[i-1], path[i])
                     if edge not in first_order.edges:
-                        first_order.add_edge(path[i-1], path[i], weight=freq)
+                        first_order.add_edge(edge[0], edge[1], weight=freq)
                     else:
-                        first_order[path[-1]][path[i]]['weight'] += freq
+                        first_order.edges[edge]['weight'] += freq
 
                 # Skip a path if its length is less than k
                 if len(path)-1 < k:
